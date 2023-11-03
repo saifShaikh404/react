0f3f14caaef319a373ac07b3334fc510e1b9c5c1
saifShaikh404 <132943658+saifShaikh404@users.noreply.github.com>
2023-11-03 16:50:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import Video from './components/Video';
+import data from "./data.json";
+import PlayButton from './components/PlayButton';
+
+function App() {
+  return (
+    <>
+      <h1>Image gallary</h1>
+
+      <div className="container">
+        {
+          data.map((ele) => {
+            return (
+              <Video 
+                key={ele.id} 
+                title={ele.title}
+                channel={ele.channel}
+                views={ele.views}
+                duration={ele.duration}
+                image={ele.image}
+                id={ele.id}
+                verified={ele.verified}>
+                  <PlayButton 
+                    value1={() => console.log("Playing... ", ele.title)} 
+                    value2={() => console.log("Paused... ", ele.title)} >
+                      {ele.title}
+                  </PlayButton>
+              </Video>
+            )
+          })
+        }
+
+        
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PlayButton.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './PlayButton.css'
+
+const PlayButton = ({value1, value2,children}) => {
+
+    // For Method 2
+    // function handleClick(){
+    //     console.log("clicked")
+    // }
+
+    let authentication = false;
+    function handleClickEvent(){
+        if(authentication) {
+            value2()
+        } else {
+            value1()
+        }
+        authentication = !authentication
+    }
+
+    return (
+        <>
+        <div className="button">
+            {/* <button onClick={() => console.log("clicked")}> Click Me!!</button> */} {/* First Method :- we can use () => callback function to handle events. If we put onlly console log inside onclick it will execute not worked with click */}
+
+            {/* <button onClick={handleClick}> Click Me!!</button> */} {/* In this method we created a function to handle event. We use handleClick inside onClick instead handleClick(). If we used handleClick() then it is executed and not worked with onclick */}
+
+            {/* Original Button  */}
+            {/* <button onClick={handleClickEvent}>Play || {children}</button> */}
+
+            {/* Practicing changing dom with conditional rendering element  */}
+            <button onClick={handleClickEvent}>{authentication ? "Play ||" : "Paused >"} {children}</button>
+
+        </div>
+        </>
+    )
+}
+
+export default PlayButton
